83ec638b0d96c891c636eca954f3b633029c73cd
eeezgame <eeezgame@163.com>
2021-02-23 07:48:10 +0800

    对接后台

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,20 @@ module.exports = {
         plugins: [require("tailwindcss"), require("autoprefixer")]
       }
     }
+  },
+  devServer: {
+    // development server port 8000
+    port: 8876,
+    // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
+    proxy: {
+      "/api": {
+        target: "http://121.196.167.199:4000",
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/"
+        }
+      }
+    }
   }
 };
