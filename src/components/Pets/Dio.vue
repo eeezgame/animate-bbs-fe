4fc026c9b1cd47652dee6fd5a9bda3db7d6c01ed
Runki <63237008+eeezgame@users.noreply.github.com>
2021-03-23 18:07:04 +0800

    宠物dio组件

---
+++ src/components/Pets/Dio.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dio"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      imgUrl: require("@/assets/dio.png")
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+@keyframes run {
+  0% {
+    background-position: 0 0;
+  }
+  15% {
+    background-position: -30px 0;
+  }
+  30% {
+    background-position: -60px 0;
+  }
+  45% {
+    background-position: -90px 0;
+  }
+  60% {
+    background-position: -120px 0;
+  }
+  75% {
+    background-position: -150px 0;
+  }
+  80% {
+    background-position: -180px 0;
+  }
+  100% {
+    background-position: -210px 0;
+  }
+}
+.dio {
+  height: 70px;
+  width: 30px;
+  background: url(../../assets/dio.png);
+  animation: run 1s steps(1) 0s infinite both;
+}
+</style>
